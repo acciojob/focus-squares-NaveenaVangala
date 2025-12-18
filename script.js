d90cbf9b0b25079fed54f8a03e65fb6ea974fdf4
NaveenaVangala <naveenav2511@gmail.com>
2025-12-18 12:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+let x=document.querySelectorAll('.square');
+        let a=document.querySelector('#square1');
+        let b=document.querySelector('#square2');
+        let c=document.querySelector('#square3');
+        a.addEventListener('mouseover', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=a)
+                x[i].style.backgroundColor= "#6F4E37";
+            }
+        });
+        b.addEventListener('mouseover', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=b)
+                x[i].style.backgroundColor= "#6F4E37";
+            }
+        });
+        c.addEventListener('mouseover', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=c)
+                x[i].style.backgroundColor= "#6F4E37";
+            }
+        });
+        a.addEventListener('mouseout', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=a)
+                x[i].style.backgroundColor= "#E6E6FA";
+            }
+        });
+        b.addEventListener('mouseout', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=b)
+                x[i].style.backgroundColor= "#E6E6FA";
+            }
+        });
+        c.addEventListener('mouseout', ()=>{
+            for(let i=0;i<x.length;i++){
+                if(x[i]!=c)
+                x[i].style.backgroundColor= "#E6E6FA";
+            }
+        });
